3269761b948733349e196d68988f09aa1bc4195b
bardarovagergana <bardarovagergana@gmail.com>
2023-01-30 23:51:12 +0200

    sqare

---
+++ name.js
@@ -0,0 +1,10 @@
+function name ()
+{
+    let name;
+    let firstName= "Gergana";
+    let lastName = "Bardarova";
+    let age=Number (35);
+    let str=firstName+" "+lastName+" "+age;
+    console.log=str;
+}
+name();
---
+++ sqareArea.js
@@ -0,0 +1,8 @@
+function sqareaArea (input)
+{
+    let a=Number(input[0]);
+    let area=a*a;
+    console.log(area);
+
+}
+sqareaArea("5");
