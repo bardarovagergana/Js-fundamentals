3f7c086bdb0fa0789a5950af39fc5b1ef9aae94a
bardarovagergana <bardarovagergana@gmail.com>
2023-02-06 00:52:29 +0200

    age

---
+++ .vscode/concatenateData.js
@@ -0,0 +1,10 @@
+function concatenateData (input, input2, input3,input4)
+{
+    let name= input;
+    let lastName=input2;
+    let age=Number(input3);
+    let city=input4;
+    console.log (`You are ${name} ${lastName} , a ${age}-years old person from ${city}`)
+    
+}
+concatenateData ("Gergana" ,"Bardarova" ,"35" ,"Razgrad")
