6a863b0576ba50530b20fdb5dc4e5e3fc391562f
bardarovagergana <bardarovagergana@gmail.com>
2023-02-17 19:31:53 +0200

    figure

---
+++ password.js
@@ -0,0 +1,14 @@
+function solve (arg)
+{
+    let a=arg;
+    if (a==="s3cr3t!P@ssw0rd")
+    {
+        console.log("Welcome");
+    }
+    else
+    {
+        console.log("Wrong password!");
+    }
+
+}
+solve("s3cr3t!P@ssw0rd")
