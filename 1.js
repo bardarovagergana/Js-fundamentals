95d616d153c76f9defc98ac3c9d3b14a2ae47a10
bardarovagergana <bardarovagergana@gmail.com>
2023-03-22 13:12:54 +0200

    jjj

--- 1.js
+++ 1.js
@@ -1,18 +1,33 @@
-function vowelsNum (input){
-    let text = input [0];
-    let sum = 0;
-    for ( let i = 0 ; i < text.length ; i++ ){
-        let ch = text[i];
-        switch(ch){
-            case "a": sum += 1;break;
-            case "e": sum += 2; break;
-            case "i": sum += 3; break;
-            case "o": sum += 4; break;
-            case "u":sum += 5; break;
+function cleverLily (input){
+let count = Number (input [0]);
+let num = Number (input[1]);
+let p1 = 0;
+let p2 = 0;
+let p3 = 0;
+let p4 = 0;
+let p5 = 0;
+for (let i = 1 ; i <= count ; i++){
+ num = Number (input[i]);
 
-        }
-    }
-    console.log(sum);
+ if ( num < 200){
+p1 ++;
+ }else if (num <= 399) {
+    p2 ++;
+ }else if (num <= 599){
+    p3 ++;
+ }else if (num <= 799){
+    p4 ++;
+ } else {
+    p5++;
+ }
 
 }
-vowelsNum (["hello"])
+console.log (`${(p1 / count * 100 ).toFixed(2)}%`);
+console.log (`${(p2 / count * 100).toFixed(2) }%`);
+console.log (`${(p3 / count * 100).toFixed(2) }%`);
+console.log (`${(p4 / count * 100).toFixed(2) }%`);
+console.log (`${(p5 / count * 100).toFixed(2) }%`);
+}
+histogram (["7","800","801","250","199","399","599","799"])
+
+
