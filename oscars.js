c863b0bd26bdd5356ef3f6cf46a621a3cc17852d
bardarovagergana <bardarovagergana@gmail.com>
2023-03-24 22:47:37 +0200

    llll

--- oscars.js
+++ oscars.js
@@ -1,29 +1,40 @@
-function oscars (input) {
+function trekkingMania (input) {
 
-    let name = input [0];
-    let startingPoints = Number (input[1]);
+    let groupNum = Number (input [0]);
+    let sum = 0;
+    let persent =0;
+    let ch = 0;
+    let hora = 0;
 
 
-    for ( let i = 3 ; i < input.length ; i++ ){
-
-        if (i % 2 !== 0){
-            let points = i +1;
-            let total = input[i].length * Number (input [points]) / 2;
-            startingPoints += total;
-        }
-        if (startingPoints > 1250.5 ){
-            break;
+    for ( let i = 1 ; i < input.length ; i++ ){
+        ch = input[i];
+        sum += Number (ch); 
     }
-}
-    if (startingPoints < 1250.5 ){
-    console.log(`Sorry, ${name} you need ${(1250.5 - startingPoints).toFixed(1)} more!`);
-}
-else {
-                console.log (`Congratulations, ${name} got a nominee for leading role with ${(startingPoints).toFixed(1)}!`)
+        for ( let i = 1 ; i < input.length ; i++ ){
+        if (ch <= 5){
+            hora += ch;
+            persent = ch / sum * 100;
+            console.log(`${persent.toFixed(2)}%`)
+        }
+        else if (ch > 6 && ch <= 12) {
+            persent = ch / sum * 100;
+         console.log(`${persent.toFixed(2)}%`)
+        }
+        else if (ch >= 13 && ch <= 25){
+            persent = ch / sum *100;
+            console.log(`${persent.toFixed(2)}%`)
+        }
+        else if (ch >= 26 && ch <= 40){
+            persent = ch / sum *100;
+            console.log(`${persent.toFixed(2)}%`)
+        }
+        else if (ch >= 41){
+            persent = ch / sum *100;
+            console.log(`${persent.toFixed(2)}%`)
+        }
 
     }
 }   
-oscars (["Sandra Bullock","340","5","Robert De Niro","50",
-"Julia Roberts","40.5","Daniel Day-Lewis","39.4",
-"Nicolas Cage","29.9",
-"Stoyanka Mutafova","33"])
+trekkingMania (["10","10","5","1","100","12","26","17","37","40","78"])
+
